94052dde84ac6fcefff585a02f0d804b920b3dc5
Ritesh123-git <67538859+Ritesh123-git@users.noreply.github.com>
2023-02-11 23:31:55 +0530

    email api in java

---
+++ src/main/java/com/email/SpringEmailDemoApplication.java
@@ -0,0 +1,23 @@
+package com.email;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+
+
+
+@SpringBootApplication
+public class SpringEmailDemoApplication {
+
+	@Autowired
+	private EmailSenderService senderService;
+	public static void main(String[] args) {
+		SpringApplication.run(SpringEmailDemoApplication.class, args);
+	}
+	@EventListener(ApplicationReadyEvent.class)
+	public void sendMail() {
+		senderService.sendSimpleEmail("riteshkumar96977@gmail.com","Valentines Day", "Ritesh loves aparna");
+	}
+}
